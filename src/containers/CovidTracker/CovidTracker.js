89feb08900f1f25b5c56c229c9f2d4b6403bae35
centek134 <centek134@wp.pl>
2021-04-16 16:52:53 +0200

    Added a way to display data for SmallChart, made code optimization

--- src/containers/CovidTracker/CovidTracker.js
+++ src/containers/CovidTracker/CovidTracker.js
@@ -8,21 +8,22 @@ import SmallChart from '../../components/SmallChart/SmallChart.js';
 class CovidTracker extends Component{
 
     componentDidMount = () => {
-        fetch("https://api.covid19api.com/summary",{
+        fetch('https://api.covid19api.com/summary',{
             method: 'GET',
-            redirect: 'follow'})
+            redirect: 'follow'
+            })
             .then(response => response.json())
             .then(result => {
-                console.log(result)
                 let countrySlug = [];
+                console.log(result);
                 for(let countryId of result.Countries){
-                    countrySlug.push(countryId.Slug)       //saving countryIDs so i can add them ass
+                    countrySlug.push(countryId.Slug)       //saving countryIDs so i can add them as option in select
                 };
                 this.setState({
                     stats:result,
                     countryId: countrySlug,
                     didMountRender: true
-                })
+                });
             })
             .catch(error => console.log('error', error));
     }
@@ -31,70 +32,56 @@ class CovidTracker extends Component{
     state = {
         stats : null,
         countryId: null,
-        didMountRender: false,
+        actualCountry:null,
+        didMountRender: false,  //checks if we fetched data, so we can render country names in select tag
         last7Days:{
             death: null,
-            newCases:null,
+            activeCases:null,
             recover:null
         }
-    }
-
-
+    };
 
     fetchDataForCharts = (countryName) => {
-            var cases = [];
-            var deaths = [];
-            var recovered = [];
-          fetch(`https://api.covid19api.com/total/country/${countryName}/status/confirmed`, {
+        var active = [];
+        var deaths = [];
+        var recovered = [];
+
+
+        fetch(`https://api.covid19api.com/total/country/${countryName}`, {
             method: 'GET',
             redirect: 'follow'
           })
             .then(response => response.json())
             .then(result => {
-                for(let i = result.length - 1; i> result.length - 8; i--){
-                    cases.push(result[i].Cases);
-                }})
-            .catch(error => console.log('error', error));
-
-            fetch(`https://api.covid19api.com/total/country/${countryName}/status/recovered`, {
-                method: 'GET',
-                redirect: 'follow'
-              })
-                .then(response => response.json())
-                .then(result => {
-                    for(let i = result.length - 1; i> result.length - 8; i--){
-                        deaths.push(result[i].Cases);
-                    }})
-                .catch(error => console.log('error', error));
-
-                fetch(`https://api.covid19api.com/total/country/${countryName}/status/deaths`, {
-                    method: 'GET',
-                    redirect: 'follow'
-                  })
-                    .then(response => response.json())
-                    .then(result => {
-                        for(let i = result.length - 1; i> result.length - 8; i--){
-                            recovered.push(result[i].Cases);
-                        }
-                    })
-                    .catch(error => console.log('error', error));
-
+                for(let i = result.length - 7; i < result.length; i++){
+                    active.push(result[i].Active);
+                    deaths.push(result[i].Deaths);
+                    recovered.push(result[i].Recovered);
+                }
+                console.log(result);
                 this.setState({last7Days:{
-                    newCases:cases,
+                    activeCases: active,
                     death: deaths,
                     recover: recovered }
-                })
-    }
+                });
+            })
+            .catch(error => console.log('error', error));
+    };
+
+
 
     render(){
         return(
             <div className ="wrapp">
-                <Header shouldRender = {this.state.didMountRender} selectOptions = {this.state.countryId}/>
+                <Header
+                    countrySwitch = {(e) => this.fetchDataForCharts(e.target.value)}
+                    shouldRender = {this.state.didMountRender}
+                    selectOptions = {this.state.countryId}/>
                 <main>
-                    <section onClick = {() => console.log("death" + this.state.last7Days.death, "cases" + this.state.last7Days.newCases, "rec" + this.state.last7Days.recover)} className = "sml-chart">
-                        <SmallChart click={() => this.fetchDataForCharts("afghanistan")} title = "Dzienne zachorowania"/>
-                        <SmallChart title = "Dzienne zgony"/>
-                        <SmallChart title = "Całkowite przypadki"/>
+                    <section onClick = {() => console.log("death " + this.state.last7Days.death, "cases " + this.state.last7Days.activeCases, "rec " + this.state.last7Days.recover)} className = "sml-chart">
+                        <SmallChart ourData = {this.state.last7Days.activeCases} title = "Active Cases"/>
+                        <SmallChart ourData = {this.state.last7Days.death} title = "Total Deaths"/>
+                        <SmallChart ourData = {this.state.last7Days.recover} title = "Total Recovered"/>
                     </section>
                     <section className = "big-chart">
                         <BigChart/>
