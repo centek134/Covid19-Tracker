63b9bf6498e31b16b5d2ddd21bfe7dea3ed2e9d0
centek134 <centek134@wp.pl>
2021-04-16 16:49:53 +0200

    Added function to display fetched data in SmallChart

--- src/components/SmallChart/SmallChart.js
+++ src/components/SmallChart/SmallChart.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import './SmallChart.css';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-
-
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 
 const SmallChart = (props) => {
-    const data = [{name: 'Page A', uv: 800, pv: 300, amt: 2400},{name: 'Page b', uv: 400, pv: 250, amt: 2400},{name: 'Page c'},{name: 'Page c'}, {name: 'Page c'}];
+    let data = [];
+    if(props.ourData !== null){   
+        for(let item of props.ourData){
+            data.push({
+                name: 'Page A',
+                uv: item
+            });
+        };
+    };
+
     return(
         <article onClick={props.click} className = "smlchart-cont">
             <h2>{props.title}</h2>
             
             <LineChart width = {450} height = {300} data={data}>
                 <Line type="monotone" dataKey = "uv" stroke="#8884d8" />
-                <Line type="monotone" dataKey = "pv" stroke="#5323d8" />
                 <CartesianGrid stroke="#ccc" />
                 <XAxis dataKey="name" />
                 <YAxis />
+                <Tooltip/>
             </LineChart>
         </article>
     );
