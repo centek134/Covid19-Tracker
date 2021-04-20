92ce45704584a757e6c0a5cfd0684a6f014e552f
centek134 <centek134@wp.pl>
2021-04-20 14:54:02 +0200

    Added date, component now can be responsive

--- src/components/SmallChart/SmallChart.js
+++ src/components/SmallChart/SmallChart.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import './SmallChart.css';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { ResponsiveContainer ,LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 
 const SmallChart = (props) => {
     let data = [];
+    let date = props.date;
     if(props.ourData !== null){   
         for(let item of props.ourData){
             data.push({
-                name: 'Page A',
-                uv: item
+                total: item
+                
             });
         };
+        for(let i = 0; i < data.length; i++){
+             data[i].date = date[i];
+        }
+        
+      //  data[data.length-1].name = 
+      console.log(data);
     };
 
     return(
         <article onClick={props.click} className = "smlchart-cont">
             <h2>{props.title}</h2>
-            
-            <LineChart width = {450} height = {300} data={data}>
-                <Line type="monotone" dataKey = "uv" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip/>
-            </LineChart>
+            <div style = {{width: '90%', height: '75%'}}>
+                <ResponsiveContainer>
+                    <LineChart  data={data}>
+                        <Line type="monotone" dataKey = "total" stroke="#8884d8" />
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis hide = "true" dataKey = "date" />
+                        <YAxis />
+                        <Tooltip/>
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
         </article>
     );
 };
