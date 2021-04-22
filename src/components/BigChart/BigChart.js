767c9100a7d331e2af743c57fc25838beda15006
centek134 <centek134@wp.pl>
2021-04-22 12:07:19 +0200

    Switched Chart type, added legend chart now display actual fetched data.

--- src/components/BigChart/BigChart.js
+++ src/components/BigChart/BigChart.js
@@ -1,78 +1,34 @@
 import React from 'react';
+import { PieChart, Pie, ResponsiveContainer, Legend } from 'recharts';
 import './BigChart.css';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 const BigChart = (props) => {
+
+  const data = [
+    { name: 'Confirmed Cases', value: props.case, fill: "#ff4800" },
+    { name: 'Deaths', value: props.death, fill: "#000000" },
+    { name: 'Recovered', value: props.recover, fill: "#15ff00" }
+  ];
     return(
         <article className = "bigchart-cont">
-            <h2>Last 7 days</h2>
+            <h2>Total</h2>
             <div style ={{width:"90%", height: "80%"}}>
-                <ResponsiveContainer>
-                    <BarChart
-                    width={600}
-                    height={300}
+              <ResponsiveContainer>
+                <PieChart width={400} height={450}>
+                  <Pie
+                    dataKey="value"
+                    startAngle={0}
+                    endAngle={360}
                     data={data}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                    >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="pv" fill="#8884d8" />
-                    <Bar dataKey="uv" fill="#82ca9d" />
-                    </BarChart>
-                </ResponsiveContainer>
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={150}
+                    fill="#8884d8"
+                    label
+                  />
+                   <Legend align = "left" height={36}/>
+                </PieChart>
+              </ResponsiveContainer>
             </div>
         </article>
     );
